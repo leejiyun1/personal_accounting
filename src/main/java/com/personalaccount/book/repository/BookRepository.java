d80cb637fdf9be43d268e7711091a7e44057e7f6
leejiyun1 <poi20701556@gmail.com>
2025-10-15 07:25:31 +0900

    add BookRepository

---
+++ src/main/java/com/personalaccount/book/repository/BookRepository.java
@@ -0,0 +1,23 @@
+package com.personalaccount.book.repository;
+import com.personalaccount.book.entity.Book;
+import com.personalaccount.book.entity.BookType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByUserIdAndActive(Long userId, Boolean isActive);
+
+    Optional<Book> findByUserIdAndBookTypeAndIsActive(
+            Long userId,
+            BookType bookType,
+            Boolean isActive
+    );
+
+    long countByUserIdAndIsActive(Long userId, Boolean isActive);
+
+    Optional<Book> findByIdAndIsActive(Long id, Boolean isActive);
+}
