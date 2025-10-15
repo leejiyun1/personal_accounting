44a8986b06f2fde8cacc3311ee5ff9973afb7a77
leejiyun1 <poi20701556@gmail.com>
2025-10-15 10:53:26 +0900

    add AccountRepository

---
+++ src/main/java/com/personalaccount/account/repository/AccountRepository.java
@@ -0,0 +1,22 @@
+package com.personalaccount.account.repository;
+
+import com.personalaccount.account.entity.Account;
+import com.personalaccount.account.entity.AccountType;
+import com.personalaccount.book.entity.BookType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    List<Account> findByBookTypeAndAccountTypeAndIsActive(
+            BookType bookType,
+            AccountType accountType,
+            Boolean isActive
+    );
+
+    Optional<Account> findByCode(String code);
+
+    List<Account> findByBookTypeAndIsActive(BookType bookType, Boolean isActive);
+}
