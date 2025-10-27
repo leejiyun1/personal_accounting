4e3e92a076e2d8fac677479870b0825656e0fe4d
leejiyun1 <poi20701556@gmail.com>
2025-10-27 09:54:16 +0900

    feat: add AnalysisRequest DTO

---
+++ src/main/java/com/personalaccount/analysis/dto/request/AnalysisRequest.java
@@ -0,0 +1,20 @@
+package com.personalaccount.analysis.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AnalysisRequest {
+
+    @NotNull(message = "장부 ID는 필수입니다")
+    private Long bookId;
+
+    @NotNull(message = "조회 월은 필수입니다")
+    @Pattern(regexp = "\\d{4}-\\d{2}", message = "YYYY-MM 형식이어야 합니다")
+    private String yearMonth;
+}
